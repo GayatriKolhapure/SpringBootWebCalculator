cfe3f1c4f98e538180060623e0a4c67e5686b920
Gayatri_Kolhapure <kgayatri1604@gmail.com>
2025-08-29 13:08:53 +0530

    Initial commit

---
+++ src/test/java/org/gayatri/SpringBootWebCalculator/SpringBootWebCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gayatri.SpringBootWebCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
