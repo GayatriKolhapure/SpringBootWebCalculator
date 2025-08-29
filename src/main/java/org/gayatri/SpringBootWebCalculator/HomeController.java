e32a2937f219004ffe88df5f5ecc2eb3d11016c6
Gayatri_Kolhapure <kgayatri1604@gmail.com>
2025-08-29 14:33:12 +0530

    done using sessionAttribute

--- src/main/java/org/gayatri/SpringBootWebCalculator/HomeController.java
+++ src/main/java/org/gayatri/SpringBootWebCalculator/HomeController.java
@@ -1,5 +1,7 @@
 package org.gayatri.SpringBootWebCalculator;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,5 +12,18 @@ public class HomeController {
     public String home(){
         System.out.println("home method called.....");
         return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest request, HttpSession session){
+//        System.out.println("add method called.....");
+
+        int n1 = Integer.parseInt(request.getParameter("num1"));
+        int n2 = Integer.parseInt(request.getParameter("num2"));
+        int result = n1+n2;
+
+        session.setAttribute("result", result);
+
+        return "result.jsp";
     }
 }
--- src/main/webApp/index.jsp
+++ src/main/webApp/index.jsp
@@ -1,7 +1,24 @@
 <%@page language="java" %>
 
 <html>
-    <body>
-        <h2>Hello World......!</h2>
-    </body>
-</html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+ <h2>Add Two Numbers</h2>
+
+  <form id="addForm" class="card" action="add">
+    <label for="a">First number</label>
+    <input type="number" id="a" placeholder="e.g., 12" name="num1" required />
+
+    <label for="b">Second number</label>
+    <input type="number" id="b" placeholder="e.g., 8" name="num2" required />
+
+    <button class="primary" id="addBtn">Add</button>
+    <div id="msg" class="result"></div>
+  </form>
+  </body>
+  <html>
